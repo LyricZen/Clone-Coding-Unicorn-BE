d02b74f011fbc9bcc7cb9d98c387a35372064fab
Jisu Lee <jisulee.shsf@gmail.com>
2024-01-20 01:00:28 +0900

    feat: entity 추가

--- src/main/java/com/sparta/clonecodingunicorn/domain/member/entity/Member.java
+++ src/main/java/com/sparta/clonecodingunicorn/domain/member/entity/Member.java
@@ -1,5 +1,58 @@
 package com.sparta.clonecodingunicorn.domain.member.entity;
 
-public class Member {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "members")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String passwordConfirm;
+
+    @Column
+    private String name;
+
+    @Column
+    private Integer birthYear;
+
+    private String gender;
+
+    @Column
+    private String job;
+
+    @Column
+    private String interestArea;
+
+    @Column
+    private Boolean deleteMember;
+
+    @Column
+    private Boolean subscribeAgree;
+
+    public Member(Long memberId, String email, String password, String passwordConfirm, String name, Integer birthYear, String gender, String job, String interestArea, Boolean deleteMember, Boolean subscribeAgree) {
+        this.memberId = memberId;
+        this.email = email;
+        this.password = password;
+        this.passwordConfirm = passwordConfirm;
+        this.name = name;
+        this.birthYear = birthYear;
+        this.gender = gender;
+        this.job = job;
+        this.interestArea = interestArea;
+        this.deleteMember = deleteMember;
+        this.subscribeAgree = subscribeAgree;
+    }
 }
