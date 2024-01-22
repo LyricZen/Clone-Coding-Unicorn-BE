98a095c26a4990dc4e2a599078b55c47a05b3eb5
Jisu Lee <jisulee.shsf@gmail.com>
2024-01-22 16:27:49 +0900

    fix: 엔드포인트 수정

--- src/main/java/com/sparta/clonecodingunicorn/domain/member/controller/MemberController.java
+++ src/main/java/com/sparta/clonecodingunicorn/domain/member/controller/MemberController.java
@@ -6,13 +6,10 @@ import com.sparta.clonecodingunicorn.domain.member.service.MemberService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/member")
+@RequestMapping("/api")
 public class MemberController {
     private final MemberService memberService;
 
